e55bdab55c85d2cec660d836fe651463d1eb6ede
syndr <syndr@ultroncore.net>
2024-11-30 23:36:55 -0700

    Switch base to bazzite desktop
    Also install AD domain membership dependencies

--- build.sh
+++ build.sh
@@ -7,17 +7,6 @@ RELEASE="$(rpm -E %fedora)"
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+rpm-ostree install neovim zsh \
+  adcli oddjob oddjob-mkhomedir sssd-ad realmd samba-common-tools
 
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
